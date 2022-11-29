6abfe31342875cf1dd3d664ba5a69c197c5ddeaa
Konstantin Karpusha <karpusharu@gmail.com>
2022-11-29 04:48:14 +0300

    working skript added

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,28 @@ let popupBg = document.querySelector('.popup__bg');
 let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__close-button');
 
-openPopupButton.addEventListener('click', function(){
-    popupBg.classList.add('popup__bg_active');
-})
-
-closePopupButton.addEventListener('click', function(){
+function closePopup(){
     popupBg.classList.remove('popup__bg_active');
-})
+}
+
+function openPopup(){
+    popupBg.classList.add('popup__bg_active');
+}
+
+openPopupButton.addEventListener('click', openPopup);
+
+closePopupButton.addEventListener('click', closePopup);
 
 let formElement = document.querySelector('.popup');
     let nameInput = document.querySelector('.popup__input_name');
     let jobInput = document.querySelector('.popup__input_job');
-    let name = document.querySelector('.profile__name');
-    let job = document.querySelector('.profile__job-description');
+
     function handleFormSubmit (evt) {
         evt.preventDefault();
+        let name = document.querySelector('.profile__name');
+        let job = document.querySelector('.profile__job-description');
         name.textContent = nameInput.value;
         job.textContent = jobInput.value;
+        closePopup();
     }
 formElement.addEventListener('submit', handleFormSubmit);
