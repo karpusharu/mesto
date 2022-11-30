c99270b08cae04f8c479f1e004d762baf347dbb8
Konstantin Karpusha <karpusharu@gmail.com>
2022-12-01 02:16:41 +0300

    Исправть ошибки Ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,30 @@
-let popupBg = document.querySelector('.popup');
-let openPopupButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button');
+const popupBg = document.querySelector('.popup');
+let name = document.querySelector('.profile__name');
+let job = document.querySelector('.profile__job-description');
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
 
-function closePopup(){
+function closePopup() {
     popupBg.classList.remove('popup_active');
 }
 
-function openPopup(){
+function openPopup() {
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
     popupBg.classList.add('popup_active');
 }
 
-openPopupButton.addEventListener('click', openPopup);
-
-closePopupButton.addEventListener('click', closePopup);
-
 let formElement = document.querySelector('.popup__main');
-    let nameInput = document.querySelector('.popup__input_name');
-    let jobInput = document.querySelector('.popup__input_job');
+let nameInput = document.querySelector('.popup__input_type-name');
+let jobInput = document.querySelector('.popup__input_type-job');
 
-    function handleFormSubmit (evt) {
-        evt.preventDefault();
-        let name = document.querySelector('.profile__name');
-        let job = document.querySelector('.profile__job-description');
-        name.textContent = nameInput.value;
-        job.textContent = jobInput.value;
-        closePopup();
-    }
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    closePopup();
+}
 formElement.addEventListener('submit', handleFormSubmit);
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
