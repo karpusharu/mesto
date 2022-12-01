cc154b40158f99144a5017b86c63f606b40ec900
Konstantin Karpusha <karpusharu@gmail.com>
2022-12-02 01:31:26 +0300

    Исправить JS код

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,28 @@
 const popupBg = document.querySelector('.popup');
-let name = document.querySelector('.profile__name');
-let job = document.querySelector('.profile__job-description');
-let popupOpenButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelector('.popup__close-button');
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__job-description');
+let btnOpenPopup = document.querySelector('.profile__edit-button');
+let btnClosePopup = document.querySelector('.popup__close-button');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+let formElement = document.querySelector('.popup__form');
 
 function closePopup() {
     popupBg.classList.remove('popup_active');
 }
 
 function openPopup() {
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
     popupBg.classList.add('popup_active');
 }
 
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_job');
-
-
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    name.textContent = nameInput.value;
-    job.textContent = jobInput.value;
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value;
     closePopup();
 }
 formElement.addEventListener('submit', handleFormSubmit);
-popupOpenButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+btnOpenPopup.addEventListener('click', openPopup);
+btnClosePopup.addEventListener('click', closePopup);
